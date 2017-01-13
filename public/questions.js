ab33d1ab41fe43b32f01aff1757853ffad590050
Cmi <miroslav.chabrecek@kistler.com>
2017-01-13 14:09:29 +0100

    Added results handling

--- public/questions.js
+++ public/questions.js
@@ -5,7 +5,7 @@ $.ajax ({
   data: { id1: getParameterByName("param1"), id2: getParameterByName("param2")} ,
   contentType: 'application/json; charset=utf-8',
   success: function(data) {
-    console.log("Asking data " + data.questions.length);
+    createTitle(data);
     for (var i = 1; i <= data.questions.length; i++) {
       appendQuestion(data.questions[i - 1], i);
     }
@@ -19,12 +19,31 @@ $('#upload').click(function() {
     type: "POST",
     url: "/quest",
     contentType: 'application/json',
-    data: JSON.stringify({}),
+    data: prepareData(),
     success: function(data) {
-       //data was sent dialog
-    }
+      alert("All OK");
+    },
+    error: function(XMLHttpRequest, textStatus, errorThrown) {
+     alert("Some error");
+   }
   })
 });
+
+function prepareData()
+{
+  var rec = [];
+
+  for (var i = 1; i <= 2; i++ ) {
+      var id = 'input[name=editList' + i +']:checked';
+      var value = $(id).val();
+      var responce = {'id': i, 'val': value };
+      rec.push(responce);
+  }
+
+  var ret = JSON.stringify({record: rec});
+  return ret;
+}
+
 
 function getParameterByName(name, url) {
     if (!url) {
@@ -38,15 +57,41 @@ function getParameterByName(name, url) {
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
+function createTitle(data) {
+  var roleName = data.questions.pop();
+  var feedbackName = data.questions.pop();
+
+  var table = new Object();
+  table["marianboda"] = "Marian Boda";
+  table["rudolfdobrovic"] = "Rudolf Dobrovic";
+  table["radokokula"] = "Rado Kokula";
+  table["jurajandris"] = "Juraj Andris";
+  table["petergejgus"] = "Peter Gejgus";
+  table["lukasondriga"] = "Lukas Ondriga";
+  table["michalsvoboda"] = "Michal Svoboda";
+
+  var name = table[feedbackName.feedback_name];
+  $('#title').append(name);
+
+  if (roleName.role_name == "selffeedback")
+  {
+      $('#title').append(" from himself");
+  }
+  else {
+    $('#title').append(" from his ");
+    $('#title').append(roleName.role_name);
+  }
+}
+
 
 function appendQuestion(question, counter) {
   var newQuestion = "<div class='question-container'>" +
-     "<div class='question-body'>" + counter + ": " + question.text + "</div>" +
+     "<div class='question-body' id='" + counter + "'>" + counter + ": " + question.text + "</div>" +
       "<div class=radio-btn>" +
 
-        "<input type='radio' name='editList' value='always'>" + "Always  " +
-        "<input type='radio' name='editList' value='never'>" +  "Never  " +
-        "<input type='radio' name='editList' value='costChange'>" + "Cost Change  " +
+        "<input type='radio' name='editList" + counter + "' value='1'>" + "Always  " +
+        "<input type='radio' name='editList" + counter + "' value='2'>" +  "Never  " +
+        "<input type='radio' name='editList" + counter + "' value='3'>" + "Cost Change  " +
       "</div>" +
      "</div>";
 
